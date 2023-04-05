d688e40fe21478004099cd028302e09066aaf1fb
WarriorsSami <sbarbutdica@gmail.com>
2023-04-05 10:50:27 +0300

    perf(neofetch): extract logic to mediator handler

---
+++ src/application/neofetch.rs
@@ -0,0 +1,44 @@
+use color_print::cprintln;
+use mediator::{Request, RequestHandler};
+
+// NeofetchEvent is a request for retrieving specifications about the OS.
+pub(crate) struct NeofetchRequest;
+
+impl Request<bool> for NeofetchRequest {}
+
+// NeofetchHandler is a handler for NeofetchRequest.
+pub(crate) struct NeofetchHandler;
+
+impl NeofetchHandler {
+    pub(crate) fn new() -> NeofetchHandler {
+        Self
+    }
+}
+
+impl RequestHandler<NeofetchRequest, bool> for NeofetchHandler {
+    fn handle(&mut self, _event: NeofetchRequest) -> bool {
+        cprintln!("<bold>
+        <w!>WWWWWWWWWWWX</><c!>Okk0</><w!>XWWXXK</><c!>00000</><w!>KNWWWWWWWWWWW</>            <w!>rouser</><b!>@</><w!>rodos-rs</>
+        <w!>WWWWWWWN</><c!>0xlcok</><w!>XWWK</><c!>xooolllllodxO</><w!>KNWWWWWWW</>            <k!>----------------</>
+        <w!>WWWWWNkl</><b!>;,</><w!>o0NWWW</><c!>0ololllolllllllox0</><w!>NWWWWW</>            <r!>OS</>: <w!>RoDOS 0.1.0</>
+        <w!>WWWNOc</><b!>,':</><w!>kNWWWWN</><c!>xllllllokxlllloolld</><w!>KWWWW</>            <r>FAT</>: <w!>FAT32</>
+        <w!>WWXd;</><b!>,,</><w!>c0WWWWWWW</><c!>Oolllllx</><w!>XN00000000</><c!>Ok0</><w!>NWW</>            <r>Disk Size</>: <w!>1.0 GB</>
+        <w!>WXo</><b!>,,,:</><w!>0WWWWWWWWN</><c!>KOkkk</><w!>0NWKkdlcccloxOKNWW</>            <r>Disk Used</>: <w!>0.1 GB</>
+        <w!>Nd</><b!>,,,,</><w!>xWWWWWWWWWWWWWWWWNx:,'</><b!>,,,,,,',</><w!>ckNW</>            <r>Allocation Unit</>: <w!>4 KB</>
+        <w!>0:</><b!>',':</><w!>0</><c!>N</><w!>NWWWWWWWWWWWWWWk;'</><b!>,,,,</><w!>::</><b!>,,,,,,</><w!>oX</>
+        <w!>x</><b!>,,,'</>:<w!>0</><c!>KkX</><w!>WWWWWWWWWWWWWk</><b!>,,,,,,</><w!>dk:</><b!>',,,'</><w!>,x</>
+        <w!>o</><b!>,,,'</><w!>;O</><c!>XddK</><w!>NWWWWWWWWW</><c!>WM</><w!>Xd</><b!>:,,;</><w!>lKKc'</><b!>,,,,</><w!>'o</>
+        <w!>d</><b!>,,,,,</><w!>l</><c!>K0oox0</><w!>KXNWNNN</><c!>XX0O</><w!>00OO</><c!>OK</><w!>WO;'</><b!>,,,,,</><w!>o</>
+        <w!>k;</><b!>',,,,</><w!>oK</><c!>0dlloddxxddoollodddk</><w!>K0c</><b!>,,,,,</><w!>',k</>
+        <w!>Kl</><b!>',,,,,</><w!>lO</><c!>Kkdolllllllllllod</><w!>OKk:</><b!>,,,,,,</><w!>'cK</>
+        <w!>WO:</><b!>,,,,,,</><w!>;lk</><c!>00OkxdddddxkO0</><w!>0kl</><b!>,,,,,,,</><w!>':OW</>
+        <w!>WWO:</><b!>,,,,,,',</><w!>:oxkOOOOOOkkdl</><b!>;,,,',,,,,</><w!>:OWW</>
+        <w!>WWWKl</><b!>,',,,,,,,',,,,,,,,'',,,,,,,,,,</><w!>l0WWW</>
+        <w!>WWWWNkc</><b!>,,,,,,,,,,,,,,,,,,,,,,,,,,</><w!>ckNWWWW</>
+        <w!>WWWWWWNOo</><b!>:,'',,,,,,,,,,,,,,,',</><w!>:oONWWWWWW</>
+        <w!>WWWWWWWWWXOxoc;</><b!>,,,,,,,,,,;</><w!>coxOXWWWWWWWWW</>
+        <w!>WWWWWWWWWWWWWX0kxdoooodxk0XWWWWWWWWWWWWW</>
+        </>");
+        true
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
-use color_print::cprintln;
+use crate::application::neofetch::{NeofetchHandler, NeofetchRequest};
+use color_print::*;
+use mediator::{DefaultMediator, Mediator};
+use std::io::Write;
+
+mod application;
+mod domain;
 
 fn main() {
-    cprintln!("<bold>
-    <w!>WWWWWWWWWWWX</><c!>Okk0</><w!>XWWXXK</><c!>00000</><w!>KNWWWWWWWWWWW</>            <w!>rouser</><b!>@</><w!>rodos-rs</>
-    <w!>WWWWWWWN</><c!>0xlcok</><w!>XWWK</><c!>xooolllllodxO</><w!>KNWWWWWWW</>            <k!>----------------</>
-    <w!>WWWWWNkl</><b!>;,</><w!>o0NWWW</><c!>0ololllolllllllox0</><w!>NWWWWW</>            <r!>OS</>: <w!>RoDOS 0.1.0</>
-    <w!>WWWNOc</><b!>,':</><w!>kNWWWWN</><c!>xllllllokxlllloolld</><w!>KWWWW</>            <r>FAT</>: <w!>FAT32</>
-    <w!>WWXd;</><b!>,,</><w!>c0WWWWWWW</><c!>Oolllllx</><w!>XN00000000</><c!>Ok0</><w!>NWW</>            <r>Disk Size</>: <w!>1.0 GB</>
-    <w!>WXo</><b!>,,,:</><w!>0WWWWWWWWN</><c!>KOkkk</><w!>0NWKkdlcccloxOKNWW</>            <r>Disk Used</>: <w!>0.1 GB</>
-    <w!>Nd</><b!>,,,,</><w!>xWWWWWWWWWWWWWWWWNx:,'</><b!>,,,,,,',</><w!>ckNW</>            <r>Allocation Unit</>: <w!>4 KB</>
-    <w!>0:</><b!>',':</><w!>0</><c!>N</><w!>NWWWWWWWWWWWWWWk;'</><b!>,,,,</><w!>::</><b!>,,,,,,</><w!>oX</>
-    <w!>x</><b!>,,,'</>:<w!>0</><c!>KkX</><w!>WWWWWWWWWWWWWk</><b!>,,,,,,</><w!>dk:</><b!>',,,'</><w!>,x</>
-    <w!>o</><b!>,,,'</><w!>;O</><c!>XddK</><w!>NWWWWWWWWW</><c!>WM</><w!>Xd</><b!>:,,;</><w!>lKKc'</><b!>,,,,</><w!>'o</>
-    <w!>d</><b!>,,,,,</><w!>l</><c!>K0oox0</><w!>KXNWNNN</><c!>XX0O</><w!>00OO</><c!>OK</><w!>WO;'</><b!>,,,,,</><w!>o</>
-    <w!>k;</><b!>',,,,</><w!>oK</><c!>0dlloddxxddoollodddk</><w!>K0c</><b!>,,,,,</><w!>',k</>
-    <w!>Kl</><b!>',,,,,</><w!>lO</><c!>Kkdolllllllllllod</><w!>OKk:</><b!>,,,,,,</><w!>'cK</>
-    <w!>WO:</><b!>,,,,,,</><w!>;lk</><c!>00OkxdddddxkO0</><w!>0kl</><b!>,,,,,,,</><w!>':OW</>
-    <w!>WWO:</><b!>,,,,,,',</><w!>:oxkOOOOOOkkdl</><b!>;,,,',,,,,</><w!>:OWW</>
-    <w!>WWWKl</><b!>,',,,,,,,',,,,,,,,'',,,,,,,,,,</><w!>l0WWW</>
-    <w!>WWWWNkc</><b!>,,,,,,,,,,,,,,,,,,,,,,,,,,</><w!>ckNWWWW</>
-    <w!>WWWWWWNOo</><b!>:,'',,,,,,,,,,,,,,,',</><w!>:oONWWWWWW</>
-    <w!>WWWWWWWWWXOxoc;</><b!>,,,,,,,,,,;</><w!>coxOXWWWWWWWWW</>
-    <w!>WWWWWWWWWWWWWX0kxdoooodxk0XWWWWWWWWWWWWW</>
-    </>");
+    let mut mediator = DefaultMediator::builder()
+        .add_handler(NeofetchHandler::new())
+        .build();
+
+    loop {
+        cprint!("<w!>rouser</><b!>@</><w!>rodos</>-rs:~$ ");
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let command = input.split_whitespace().next().unwrap();
+
+        match command {
+            "neofetch" => {
+                mediator.send(NeofetchRequest).unwrap();
+            }
+            "exit" => {
+                cprintln!("<r!>RoDOS is shutting down!</>");
+                break;
+            }
+            _ => println!("Command not found"),
+        }
+    }
 }
